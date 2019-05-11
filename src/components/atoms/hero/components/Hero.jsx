5e92a93501af08a85d8cf1eca0f05cdd90081fa9
rdo34 <ross.donohoe@sky.uk>
2019-05-11 12:27:33 +0100

    add readme, improve css

--- src/components/atoms/hero/components/Hero.jsx
+++ src/components/atoms/hero/components/Hero.jsx
@@ -5,8 +5,14 @@ export const Hero = props => {
   const { src, height, children } = props;
   if (src) {
     const HeroImage = styled.div`
-      width: 100%;
-      height: ${height || '512'}px;
+      width: 100vw;
+	    max-width: 100vw;
+      left: 50%;
+      right: 50%;
+      margin-left: -50vw;
+      margin-right: -50vw;
+	    position: relative;
+      height: ${height || '512px'};
       background-image: url(${src});
     `;
     return (
