98d6af393822005c7f654e335469f6cbf214a8eb
rdo34 <ross.donohoe@sky.uk>
2019-06-02 01:10:56 +0100

    remove story container

--- src/components/molecules/product-card/ProductCard.stories.js
+++ src/components/molecules/product-card/ProductCard.stories.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
+import {
+  withKnobs,
+  text,
+  boolean,
+  number,
+  select
+} from '@storybook/addon-knobs';
 
 import { ProductCard } from './components/ProductCard';
 import readMe from './README.md';
@@ -9,29 +15,22 @@ import readMe from './README.md';
 const exampleImgSrc =
   'https://cdn.shopify.com/s/files/1/0015/2774/2577/products/product-image-670060808_800x.jpg?v=1543901544';
 
-const Container = styled.div`
-  margin-left: 10px;
-  position: relative;
-`;
-
 storiesOf('Molecules | Product Card', module)
   .addDecorator(withKnobs)
   .add(
     'Default',
     () => (
-      <Container>
-        <ProductCard
-          product={{
-            staticId: text('Product ID', 'SOME_ID'),
-            displayName: text('Display Name', 'Jacket Potatoe'),
-            currentPrice: number('Current Price', 50),
-            imgSrc: text('Image Source', exampleImgSrc),
-            inStock: boolean('In Stock?', true)
-          }}
-          href={text('href', '/')}
-          onClick={select('onClick', [() => {}, null], null)}
-        />
-      </Container>
+      <ProductCard
+        product={{
+          staticId: text('Product ID', 'SOME_ID'),
+          displayName: text('Display Name', 'Jacket Potatoe'),
+          currentPrice: number('Current Price', 50),
+          imgSrc: text('Image Source', exampleImgSrc),
+          inStock: boolean('In Stock?', true)
+        }}
+        href={text('href', '/')}
+        onClick={select('onClick', [() => {}, null], null)}
+      />
     ),
     { notes: { markdown: readMe } }
   );
